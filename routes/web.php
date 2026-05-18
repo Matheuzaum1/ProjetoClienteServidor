c5892d8531102766d9b75ab0f85f1be1e03d4cea
Matheuzaum1 <matheusehenriquerosendo@gmail.com>
2026-05-18 10:41:48 -0300

    Route: exempt /token/refresh from CSRF so cookie-based refresh works when using web routes

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ Route::post('/usuarios', [UsuarioController::class, 'store']);
 Route::post('/usuarios/login', [AuthController::class, 'login']);
 
 Route::post('/usuarios/logout', [AuthController::class, 'logout']);
-Route::middleware('api')->post('/token/refresh', [AuthController::class, 'refresh']);
+Route::post('/token/refresh', [AuthController::class, 'refresh'])->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
 
 Route::middleware('auth:api')->group(function () {
     Route::get('/usuarios', [UsuarioController::class, 'index']);
